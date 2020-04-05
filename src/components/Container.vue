1157b19bb490fa33c68c2597ac167c512793bfe5
Rei Allen Ramos <reiallenramos@gmail.com>
2020-04-05 13:54:26 +0800

    navbar placeholder

---
+++ src/components/Container.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <Navbar />
+  </div>
+</template>
+
+<script>
+import Navbar from './Navbar.vue'
+
+export default {
+  name: 'Container',
+  components: {
+    Navbar
+  }
+}
+</script>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,44 @@
+<template>
+  <nav class="navbar">
+    <div class="container">
+      <ul class="navbar-list">
+        <li class="navbar-item">
+          <a class="navbar-link" href="#route-1">Route 1</a>
+        </li>
+        <li class="navbar-item">
+          <a class="navbar-link" href="#route-2">Route 2</a>
+        </li>
+        <li class="navbar-item">
+          <a class="navbar-link" href="#route-3">Route 3</a>
+        </li>
+        <li class="navbar-item">
+          <a class="navbar-link" href="#route-4">Route 4</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.navbar-list {
+  list-style: none;
+  margin-bottom: 0;
+}
+
+.navbar-item {
+  position: relative;
+  float: left;
+  margin-bottom: 0;
+}
+
+.navbar-link {
+  text-transform: uppercase;
+  font-size: 11px;
+  font-weight: 600;
+  letter-spacing: .2rem;
+  margin-right: 35px;
+  text-decoration: none;
+  line-height: 6.5rem;
+  color: #222;
+}
+</style>
