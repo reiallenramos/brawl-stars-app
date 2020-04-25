0033ebd3f03606fd4674565b6ff4188955bc542d
Rei Allen Ramos <reiallenramos@gmail.com>
2020-04-25 15:26:22 +0800

    move router setup to routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,32 +1,36 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 import Brawlers from './components/Brawlers.vue'
 import BrawlersIndex from './components/BrawlersIndex.vue'
 
-const routes = [
-  { path: '/', redirect: '/brawlers'},
-  { path: '/brawlers', component: Brawlers,
-    children: [
-      {
-        path: '',
-        component: BrawlersIndex,
-        name: 'brawlersIndex'
-      },
-      {
-        path: ':name',
-        component: () => import( /* webpackChunkName: "brawlerProfile" */ "./components/BrawlerProfile" ),
-        name: 'brawlerProfile'
-      }
-    ]
-  },
-  {
-    path: '/compare',
-    component: () => import( /* webpackChunkName: "compareHome" */ "./components/Compare" ),
-    name: 'compareHome'
-  },
-  {
-    path: '/clans',
-    component: () => import( /* webpackChunkName: "clansHome" */ "./components/Clans" ),
-    name: 'clansHome'
-  },
-];
+Vue.use(VueRouter);
 
-export default routes;
+export default new VueRouter({
+  routes: [
+    { path: '/', redirect: '/brawlers'},
+    { path: '/brawlers', component: Brawlers,
+      children: [
+        {
+          path: '',
+          component: BrawlersIndex,
+          name: 'brawlersIndex'
+        },
+        {
+          path: ':name',
+          component: () => import( /* webpackChunkName: "brawlerProfile" */ "./components/BrawlerProfile" ),
+          name: 'brawlerProfile'
+        }
+      ]
+    },
+    {
+      path: '/compare',
+      component: () => import( /* webpackChunkName: "compareHome" */ "./components/Compare" ),
+      name: 'compareHome'
+    },
+    {
+      path: '/clans',
+      component: () => import( /* webpackChunkName: "clansHome" */ "./components/Clans" ),
+      name: 'clansHome'
+    },
+  ]
+})
